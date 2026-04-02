97154510b4681e96bae2085c9a1664045ab5c4c8
Tayfuntacar <presidenttrumparabic14@gmail.com>
2026-04-02 22:40:54 +0200

    add color

--- scraper.js
+++ scraper.js
@@ -1,1 +1,43 @@
-var WebSocket=require('ws');var fs=require('fs');var https=require('https');var opt={hostname:'virtualbingodataprovider-volcano.xtreme.bet',path:'/hubs/messagehub/negotiate?negotiateVersion=1',method:'POST',headers:{'Origin':'https://www.volcanobet.me','Content-Type':'application/json'},rejectUnauthorized:false};function connect(){var req=https.request(opt,function(res){var body='';res.on('data',function(d){body+=d;});res.on('end',function(){var data=JSON.parse(body);var token=encodeURIComponent(data.connectionToken);var ws=new WebSocket('wss://virtualbingodataprovider-volcano.xtreme.bet/hubs/messagehub?id='+token,{headers:{'Origin':'https://www.volcanobet.me'},rejectUnauthorized:false});ws.on('open',function(){ws.send('["protocol","json","version",1]');setTimeout(function(){ws.send('{"arguments":[],"target":"SubscribeToCurrentDraw","type":1}');ws.send('{"arguments":[],"target":"SubscribeToDrawResults","type":1}');console.log('Baglandi!');},1000);});ws.on('message',function(d){var m=d.toString().replace(//g,'');try{var json=JSON.parse(m);if(json.target==='ReceivePartialResult'){var draw=json.arguments[0];var balls=draw.ballNumbers;var first=balls[0];var first5=balls.slice(0,5);var isOver=first>24?'OVER':'UNDER';console.log('ROUND:'+draw.number+' FIRST:'+first+' '+isOver);fs.appendFileSync('draws.csv',draw.number+'|'+first+'|'+isOver+'|'+first5.join(',')+'\n');}}catch(e){}});ws.on('close',function(){setTimeout(connect,3000);});ws.on('error',function(e){console.log(e.message);});});});req.on('error',function(){setTimeout(connect,3000);});req.end();}fs.writeFileSync('draws.csv','round|first|over_under|first5\n');console.log('Basliyor...');connect();
+var WebSocket = require('ws');
+var fs = require('fs');
+var https = require('https');
+var colors = {1:'Sari',9:'Sari',17:'Sari',25:'Sari',33:'Sari',41:'Sari',2:'Yesil',10:'Yesil',18:'Yesil',26:'Yesil',34:'Yesil',42:'Yesil',3:'Mavi',11:'Mavi',19:'Mavi',27:'Mavi',35:'Mavi',43:'Mavi',4:'Kirmizi',12:'Kirmizi',20:'Kirmizi',28:'Kirmizi',36:'Kirmizi',44:'Kirmizi',5:'Kahve',13:'Kahve',21:'Kahve',29:'Kahve',37:'Kahve',45:'Kahve',6:'Turuncu',14:'Turuncu',22:'Turuncu',30:'Turuncu',38:'Turuncu',46:'Turuncu',7:'Siyah',15:'Siyah',23:'Siyah',31:'Siyah',39:'Siyah',47:'Siyah',8:'Mor',16:'Mor',24:'Mor',32:'Mor',40:'Mor',48:'Mor'};
+var opt = {hostname:'virtualbingodataprovider-volcano.xtreme.bet',path:'/hubs/messagehub/negotiate?negotiateVersion=1',method:'POST',headers:{'Origin':'https://www.volcanobet.me'},rejectUnauthorized:false};
+function connect() {
+  https.request(opt, function(r) {
+    var b = '';
+    r.on('data', function(d) { b += d; });
+    r.on('end', function() {
+      var t = encodeURIComponent(JSON.parse(b).connectionToken);
+      var w = new WebSocket('wss://virtualbingodataprovider-volcano.xtreme.bet/hubs/messagehub?id=' + t, {headers:{'Origin':'https://www.volcanobet.me'},rejectUnauthorized:false});
+      var saved = {};
+      w.on('open', function() {
+        w.send('{"protocol":"json","version":1}\x1e');
+        setTimeout(function() {
+          w.send('{"arguments":["00000000-0000-0000-0000-000000000000"],"invocationId":"0","target":"SubscribeClient","type":1}\x1e');
+        }, 1000);
+      });
+      w.on('message', function(d) {
+        try {
+          var j = JSON.parse(d.toString().replace(/\x1e/g, ''));
+          if (j.target === 'ReceivePartialResult' && j.arguments[0].ballNumbers.length === 35) {
+            var a = j.arguments[0];
+            if (!saved[a.number]) {
+              saved[a.number] = 1;
+              var first = a.ballNumbers[0];
+              var ou = first > 24 ? 'OVER' : 'UNDER';
+              var renk = colors[first] || 'Bilinmiyor';
+              console.log('ROUND:' + a.number + ' FIRST:' + first + ' ' + ou + ' ' + renk);
+              fs.appendFileSync('draws.csv', a.number + '|' + first + '|' + ou + '|' + renk + '|' + a.ballNumbers.join(',') + '\n');
+            }
+          }
+        } catch(e) {}
+      });
+      w.on('close', function() { setTimeout(connect, 3000); });
+      w.on('error', function() { setTimeout(connect, 3000); });
+    });
+  }).end();
+}
+fs.writeFileSync('draws.csv', 'round|first|over_under|color|all_numbers\n');
+console.log('Basliyor...');
+connect();
